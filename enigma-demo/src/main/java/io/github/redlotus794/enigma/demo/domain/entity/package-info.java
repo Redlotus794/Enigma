cff113ddfec161c1367f017d5fb05cc61181edf5
jialong.wang <jialong.wang@smil.com>
2026-05-24 11:43:36 +0800

    refactor(docs): 精简并优化 enigma-demo PRD 大纲内容

--- enigma-demo/src/main/java/io/github/redlotus794/enigma/demo/domain/entity/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Enigma Demo 领域层实体包。
- */
-package io.github.redlotus794.enigma.demo.domain.entity;
-
--- enigma-demo/src/main/java/io/github/redlotus794/enigma/demo/domain/valueobject/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Enigma Demo 领域层值对象包。
- */
-package io.github.redlotus794.enigma.demo.domain.valueobject;
-
